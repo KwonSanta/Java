2b331592f6a642efd555d74c609e2a1ef5b8ba48
KwonSanta <KwonSanta@gmail.com>
2024-02-19 12:41:07 +0900

    처음 만들어본 자바 코드

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,12 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        System.out.println("Hello and 권성인!");
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Hi Java!");
+        System.out.println("Hola IntelliJ");
+        System.out.println("안녕 중앙정보");
+        System.out.println("배고프다.");
     }
 }
---
+++ src/Main2.java
@@ -0,0 +1,6 @@
+public class Main2 {
+    public static void main(String[] args) {
+        System.out.println("새 파일에서 인사");
+        System.out.println("안녕 새 파일");
+    }
+}
