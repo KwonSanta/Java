1b4b0d3a075bb405d337895035399f9c4b3bed53
KwonSanta <KwonSanta@gmail.com>
2024-02-20 10:19:20 +0900

    remove *.iml

---
+++ src/c02/lecture/p02type/C03Integer.java
@@ -0,0 +1,7 @@
+package c02.lecture.p02type;
+
+public class C03Integer {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/c02/sec02/LongExample.java
@@ -0,0 +1,16 @@
+package c02.sec02;
+
+public class LongExample {
+    public static void main(String[] args) {
+        long var1 = 10;
+        long var2 = 20L;
+        long var3 = 10000000000L;
+        long var4 = 10000000000L;
+
+        System.out.println("var1 = " + var1);
+        System.out.println("var2 = " + var2);
+        System.out.println("var3 = " + var3);
+        System.out.println("var4 = " + var4);
+
+    }
+}
